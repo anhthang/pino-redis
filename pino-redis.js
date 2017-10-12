37f7c5817b9d7ffbeff5713bb2787e03f6029561
Anh Thang Bui <buianhthang89@gmail.com>
2017-10-12 22:32:59 +0700

    feat: support command

--- pino-redis.js
+++ pino-redis.js
@@ -1,16 +1,15 @@
-#! /usr/bin/env node
-'use strict'
-
 const Writable = require('readable-stream').Writable
 const Redis = require('ioredis')
 const Parse = require('fast-json-parse')
 const split = require('split2')
 const pump = require('pump')
 const zlib = require('zlib')
+const minimist = require('minimist')
 
 function pinoRedis(opts) {
+    console.log('opts', opts)
     const splitter = split(function(line) {
-        var parsed = new Parse(line)
+        const parsed = new Parse(line)
         if (parsed.err) {
             this.emit('unknown', line, parsed.err)
             return
@@ -23,27 +22,27 @@ function pinoRedis(opts) {
 
     const writable = new Writable({
         objectMode: true,
-        writev: function(chunks, cb) {
-            var pipeline = redis.pipeline()
+        writev(chunks, cb) {
+            const pipeline = redis.pipeline()
             chunks.forEach(item => {
-                var body = item.chunk
+                const body = item.chunk
                 if (!body.key) throw new Error('key is required')
 
                 const ttl = body.ttl || 60
                 const gzip = !!body.gzip
                 const value = JSON.stringify(body)
 
-                var redisKey = `${body.level}:${body.key}`
+                const redisKey = `${body.level}:${body.key}`
                 if (gzip) {
                     zlib.gzip(value, (c_err, buffer) => {
                         if (!c_err) {
-                            pipeline.set(redisKey, buffer.toString('binary'), 'EX', ttl)
+                            pipeline.setex(redisKey, ttl, buffer.toString('binary'))
                         } else {
                             splitter.emit('insertError', c_err)
                         }
                     })
                 } else {
-                    pipeline.set(redisKey, value, 'EX', ttl)
+                    pipeline.setex(redisKey, ttl, value)
                 }
 
                 pipeline.exec((err, result) => {
@@ -62,31 +61,32 @@ function pinoRedis(opts) {
                 })
             })
         },
-        write: function(body, enc, cb) {
+        write(body, enc, cb) {
             if (!body.key) throw new Error('key is required')
 
             const ttl = body.ttl || 60
             const gzip = !!body.gzip
             const value = JSON.stringify(body)
 
-            var redisKey = `${body.level}:${body.key}`
+            const redisKey = `${body.level}:${body.key}`
             if (gzip) {
                 zlib.gzip(value, (c_err, buffer) => {
                     if (!c_err) {
-                        redis.set(redisKey, buffer.toString('binary'), 'EX', ttl, function(err, result) {
-                            if (!err) {
-                                splitter.emit('insert', body)
-                            } else {
-                                splitter.emit('insertError', err)
+                        redis.setex(redisKey, ttl, buffer.toString('binary'), (err, result) => {
+                                if (!err) {
+                                    splitter.emit('insert', body)
+                                } else {
+                                    splitter.emit('insertError', err)
+                                }
+                                cb()
                             }
-                            cb()
-                        })
+                        )
                     } else {
                         splitter.emit('insertError', c_err)
                     }
                 })
             } else {
-                redis.set(redisKey, value, 'EX', ttl, function(err, result) {
+                redis.setex(redisKey, ttl, value, (err, result) => {
                     if (!err) {
                         splitter.emit('insert', body)
                     } else {
@@ -104,3 +104,27 @@ function pinoRedis(opts) {
 }
 
 module.exports = pinoRedis
+
+function start(opts) {
+    if (opts.version) {
+        console.log('pino-redis', require('./package.json').version)
+        return
+    }
+
+    pump(process.stdin, pinoRedis(opts))
+}
+
+if (require.main === module) {
+    start(
+        minimist(process.argv.slice(2), {
+            alias: {
+                version: 'v',
+                help: 'h',
+                connectionUrl: 'U'
+            },
+            default: {
+                connectionUrl: 'redis://:@localhost:6379'
+            }
+        })
+    )
+}
